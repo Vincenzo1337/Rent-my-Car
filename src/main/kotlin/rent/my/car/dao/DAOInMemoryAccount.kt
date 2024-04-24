dcb9402788f2a5308a70600ea94a9471b9c15b25
Vincenzo1337 <vincentbeurskens89@gmail.com>
2024-04-24 23:04:14 +0200

    account model geupdate

--- src/main/kotlin/rent/my/car/dao/DAOInMemoryAccount.kt
+++ src/main/kotlin/rent/my/car/dao/DAOInMemoryAccount.kt
@@ -16,15 +16,16 @@ object DaoInMemoryAccount : DAOFacadeAccount {
 
 object AccountDatabase {
     val accounts = mutableListOf(
-        Account("vincent", "test1234", 1),
-        Account("kayal", "test1234", 2),
-        Account("casper", "test1234", 3),
-        Account("michael", "test1234", 4),
-        Account("herman", "test1234", 5),
-        Account("jasper", "test1234", 6),
-        Account("joost", "test1234", 7),
-        Account("jill", "test1234", 8),
-        Account("kekw", "test1234", 9),
-        Account("jax", "test1234", 10),
+        Account("vincent", "test1234", 1, "Vincent", "0612345678", "vincent@gmail.com"),
+        Account("kayal", "test1234", 2, "Kayal", "0612345678", "kayal@gmail.com"),
+        Account("casper", "test1234", 3,  "Casper", "0612345678", "casper@gmail.com"),
+        Account("michael", "test1234", 4,  "Michael", "0612345678", "michael@gmail.com",),
+        Account("herman", "test1234", 5,  "Herman", "0612345678", "herman@gmail.com"),
+        Account("jasper", "test1234", 6,  "Jasper", "0612345678", "jasper@gmail.com"),
+        Account("joost", "test1234", 7, "Joost", "0612345678", "joost@gmail.com"),
+        Account("jill", "test1234", 8, "Jill", "0612345678", "jill@gmail.com"),
+        Account("kekw", "test1234", 9,  "Kekw", "0612345678", "kekw@gmail.com"),
+        Account("jax", "test1234", 10,  "Jax", "0612345678", "jax@gmail.com"),
     )
+    var nextUserId = accounts.size + 1 // Begint bij 11
 }
